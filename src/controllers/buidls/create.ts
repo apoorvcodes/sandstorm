97c8797af60049f82eee7d177c0ac21545d84da1
apoorvcodes <tofu.develops@gmail.com>
2023-01-16 22:52:42 +0530

    feat: working on mint nft

--- src/controllers/buidls/create.ts
+++ src/controllers/buidls/create.ts
@@ -72,6 +72,6 @@ export async function buidlCreate(req: Request, res: Response) {
   res.status(200).json({
     status: 200,
     data: buidl
-  });
+  })
   console.log(buidl);
 }
---
+++ src/controllers/mint/mint.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express';
+
+export async function buidlRead(req: Request, res: Response) {
+  
+}
